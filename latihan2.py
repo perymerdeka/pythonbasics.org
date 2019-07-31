d2ab0d7452c5fa9493d74795e56aa1397d8efe57
perymerdeka <47948861+perymerdeka@users.noreply.github.com>
2019-07-31 23:14:50 +0700

    Add files via upload

---
+++ latihan2.py
@@ -0,0 +1,12 @@
+#buat program untuk menampilkan beberapa angka
+print(16)
+print(17)
+print(22)
+print(2.10)
+#buat program untuk menunjukan hasil penjumlahan 64 + 32
+print(64 + 32)
+
+#lakukan halyang sama pada no 2 tetapi disinpan dalam x dan y
+x = 64
+y = 32
+print(x + y)
